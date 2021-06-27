4120f48d0f97e1af61e2ff882c3a90a996677f5d
Santiago Bernal <exactlimon@gmail.com>
2021-06-26 19:35:07 -0500

    create basic divs

--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ import data from './data/pokemon/pokemon.js';
 console.log(example);
 
 const createLayout = () => {
-  
+  const root = document.getElementById('root');
+  const container = document.createElement('div');
+  container.id = 'container';
+  root.appendChild(container);
 }
+
+const createCard = (element) => {
+  const container = document.getElementById('container');
+  const card = document.createElement('div');
+  card.id = element.num;
+  container.appendChild(card);
+  //console.log(element);
+};
 
 const printData = (data) => {
   const pokemonArray = data['pokemon']
-  console.log(pokemonArray);
+  pokemonArray.forEach(element => createCard(element));
+  //console.log(pokemonArray);
 };
 
 const showPokemons = (data) => {
