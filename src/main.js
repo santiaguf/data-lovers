2371e22e422cb632fd8a5145b3b6266325e5a8ef
Santiago Bernal <exactlimon@gmail.com>
2021-06-29 16:57:23 -0500

    create basic card

--- src/main.js
+++ src/main.js
@@ -14,12 +14,37 @@ const createCard = (element) => {
   const container = document.getElementById('container');
   const card = document.createElement('div');
   card.id = element.num;
+  card.classList.add('card');
   container.appendChild(card);
+
+  const imgContainer = document.createElement('div');
+  imgContainer.id = `img-container-${element.num}`;
+  imgContainer.classList.add('img-container');
+  card.appendChild(imgContainer);
+
+  const img = document.createElement('img');
+  img.setAttribute('id', `img-${element.num}`);
+  img.setAttribute('src', `${element.img}`);
+  img.setAttribute('class', 'img');
+  imgContainer.appendChild(img);
+
+  const infoContainer = document.createElement('div');
+  infoContainer.id = `info-container-${element.num}`;
+  infoContainer.classList.add('info-container');
+  card.appendChild(infoContainer);
+
   //console.log(element);
 };
 
+const removeItems = () => {
+  const container = document.createElement('div');
+  container.textContent = '';
+}
+
 const printData = (data) => {
+
   const pokemonArray = data['pokemon']
+  removeItems();
   pokemonArray.forEach(element => createCard(element));
   //console.log(pokemonArray);
 };
